3c8d9a15ed05e754e4570ae21ac0a661567349d0
manuhofb <manuelhofb07@gmail.com>
2026-05-05 13:27:55 +0200

    udp finish

--- ITPDroneGUI/obj/Debug/net9.0/ITPDroneGUI.AssemblyInfo.cs
+++ ITPDroneGUI/obj/Debug/net9.0/ITPDroneGUI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ITPDroneGUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5876f0629364d83f52e367e393a2b843495ad8db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ae440bba2df93937519d55e40f977ece6ac8e43")]
 [assembly: System.Reflection.AssemblyProductAttribute("ITPDroneGUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ITPDroneGUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
